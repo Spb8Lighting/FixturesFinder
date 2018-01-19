fa572b7af13f3f9f75e657f1a99c6492fb85a7bd
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-01-19 12:51:07 +0100

    Update and document the Options table

--- private/js/11-ManageOptions.js
+++ private/js/11-ManageOptions.js
@@ -1,52 +1,56 @@
-let TableOptions = {
+let Table = {
     /**
-     * Create the "Options" Table and insert default values
-     */
-    Initialize : () => {
-        db.Options.serialize(() => {
-            /* Create and fill the Database for Options */
-            db.Options.run('CREATE TABLE `' + config.Database.Options + '` ( `' + config.OptionForm.SearchMode + '` TEXT, `' + config.OptionForm.DisplayMode + '` TEXT, `' + config.OptionForm.ParameterList + '` TEXT )')
-            db.Options.run('INSERT INTO `' + config.Database.Options + '` ( `' + config.OptionForm.SearchMode + '`, `' + config.OptionForm.DisplayMode + '`, `' + config.OptionForm.ParameterList + '`) VALUES ($SearchMode, $DisplayMode, $ParameterList)', {
-                $SearchMode : config.OptionForm.SearchMode_OrderExact,
-                $DisplayMode : config.OptionForm.DisplayMode_Full,
-                $ParameterList : config.OptionForm.ParameterList_Common
+    * "Options" Table getters and setters
+    */
+    Options : {
+        /**
+        * Create the "Options" Table and insert default values
+        * @returns {void}
+        */
+        Initialize : () => {
+            db.serialize(() => {
+                /* Create and fill the Database for Options */
+                db.run('CREATE TABLE `' + config.Database.Options + '` ( `' + config.OptionForm.SearchMode + '` TEXT, `' + config.OptionForm.DisplayMode + '` TEXT, `' + config.OptionForm.ParameterList + '` TEXT )')
+                db.run('INSERT INTO `' + config.Database.Options + '` ( `' + config.OptionForm.SearchMode + '`, `' + config.OptionForm.DisplayMode + '`, `' + config.OptionForm.ParameterList + '`) VALUES ($SearchMode, $DisplayMode, $ParameterList)', {
+                    $SearchMode : config.OptionForm.SearchMode_OrderExact,
+                    $DisplayMode : config.OptionForm.DisplayMode_Full,
+                    $ParameterList : config.OptionForm.ParameterList_Common
+                })
             })
-        })
-        return this
+            return this
+        },
+        /**
+        * Restore defaults Options in "Options" Table
+        * @returns {void}
+        */
+        Reset : () => {
+            Table.Options.Update({
+                SearchMode : config.OptionForm.SearchMode_OrderExact,
+                DisplayMode : config.OptionForm.DisplayMode_Full,
+                ParameterList : config.OptionForm.ParameterList_Common
+            })
+        },
+        /**
+        * Update "Options Table"
+        * @param {{ SearchMode: String, DisplayMode: String, ParameterList : String }} data
+        * @returns {void}
+        */
+        Update : (data) => {
+            db.serialize(() => {
+                /* Reset the options to its default */
+                db.Options.run('UPDATE `' + config.Database.Options + '` SET `' + config.OptionForm.SearchMode + '` = $SearchMode, `' + config.OptionForm.DisplayMode + '` = $DisplayMode, `' + config.OptionForm.ParameterList + '` = $ParameterList', {
+                    $SearchMode : data.SearchMode,
+                    $DisplayMode : data.DisplayMode,
+                    $ParameterList : data.ParameterList
+                })
+            })
+            return this
+        }
     },
     /**
-     * Restore defaults Options in "Options" Table
-     */
-    Reset : () => {
-        ManageOptions.Update([config.OptionForm.SearchMode_OrderExact, config.OptionForm.DisplayMode_Full, config.OptionForm.ParameterList_Common])
-        db.Options.serialize(() => {
-            /* Reset the options to its default */
-            db.Options.run('UPDATE `' + config.Database.Options + '` SET `' + config.OptionForm.SearchMode + '` = $SearchMode, `' + config.OptionForm.DisplayMode + '` = $DisplayMode, `' + config.OptionForm.ParameterList + '` = $ParameterList', {
-                $SearchMode : config.OptionForm.SearchMode_OrderExact,
-                $DisplayMode : config.OptionForm.DisplayMode_Full,
-                $ParameterList : config.OptionForm.ParameterList_Common
-            })
-        })
-        return this
-    },
-    /**
-     * Update "Options Table" > "Search Mode", "DisplayMode", "ParameterList"
-     * @param {array} params => "Search Mode", "DisplayMode", "ParameterList"
-     */
-    Update : (params) => {
-        db.Options.serialize(() => {
-            /* Reset the options to its default */
-            db.Options.run('UPDATE `' + config.Database.Options + '` SET `' + config.OptionForm.SearchMode + '` = $SearchMode, `' + config.OptionForm.DisplayMode + '` = $DisplayMode, `' + config.OptionForm.ParameterList + '` = $ParameterList', {
-                $SearchMode : config.OptionForm.SearchMode_OrderExact,
-                $DisplayMode : config.OptionForm.DisplayMode_Full,
-                $ParameterList : config.OptionForm.ParameterList_Common
-            })
-        })
-        return this
-    },
-     /**
-     * Close "Options Table"
-     */
+    * Close Database
+    * @returns {void}
+    */
     Close : () => {
         db.Options.close(err => {
             if(err) {
@@ -58,3 +62,5 @@ let TableOptions = {
         return this
     }
 }
+
+Table.Options.Initialize()
