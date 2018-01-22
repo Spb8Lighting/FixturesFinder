84a4acb96323c9664dbb82c2275c73588d53bb57
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-01-22 21:31:41 +0100

    Start implementation of Select change for CSS coloring

--- private/js/99-App.js
+++ private/js/99-App.js
@@ -1,2 +1,10 @@
 /* Initialize the page content*/
 Table.Options.Initialize(DMXChannelSearch.AddChannelSearch)
+
+/* General Function */
+//TO BE UPDATED TO TAKE CARE ABOUT DYNAMIC SELECT ADD/REMOVE
+document.querySelectorAll('select').forEach( elem => {
+    elem.addEventListener('change', e => {
+        elem.setAttribute('data-option', elem.querySelector('option:selected').getAttribute('value'))
+    })
+})
