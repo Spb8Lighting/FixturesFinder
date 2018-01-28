98039557756f486bd4081f8d25202e36bacb4b00
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-01-29 00:01:50 +0100

    Fix Build issue with SQLITE

--- private/js/04-DB-Init.js
+++ private/js/04-DB-Init.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const dblocation = `db.sqlite3`
+const dblocation = process.env.NODE_ENV ? `${__dirname}/../../db.sqlite3` : `${__dirname}/../../../db.sqlite3`
 ,   db = new sqlite3.Database(dblocation, err => {
     if(err) {
         return console.error(err.message)
