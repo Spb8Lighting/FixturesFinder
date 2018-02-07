1722bfc0c0c1f6e41f7fddae1a9961cf347a819a
GUIBLAIN <NG0051B0@EU.AIRBUS.CORP>
2018-02-07 17:32:22 +0100

    Optimize Navigation.js

--- public/js/app.js
+++ public/js/app.js
@@ -54,22 +54,26 @@ $aLink.forEach(elem => {
     elem.addEventListener('click', e => {
         e.preventDefault()
         let PageName = elem.getAttribute('href')
-        $aLink.forEach(elem => {                                                // Remove .active on links
+        $aLink.forEach(elem => {
+            // Remove .active on links
             if (elem.getAttribute('href') == PageName) {
                 elem.classList.add('active')
             } else {
                 elem.classList.remove('active')
             }
         })
-        AllDivs.forEach(elem => {            // Show only the wanted content
-            if (elem.getAttribute('id') == PageName) {
+        AllDivs.forEach(elem => {
+            // Show only the wanted content
+            if (elem.id == PageName) {
                 elem.classList.remove('hide')
             } else {
                 elem.classList.add('hide')
             }
         })
-        $h1.innerHTML = `Fixtures Finder/${PageName} - v${config.Version}`      // Update Title
-        elem.blur()                 // Remove :active on link
+        // Update Title
+        $h1.innerHTML = `Fixtures Finder/${PageName} - v${config.Version}`
+        // Remove :active on link
+        elem.blur()
     }, { passive: false })
 })
 const sqlite3 = require('sqlite3').verbose()
