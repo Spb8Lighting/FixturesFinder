cb518842ec349d6795a5735fc11863a232c35574
GUIBLAIN <NG0051B0@EU.AIRBUS.CORP>
2018-02-07 17:33:06 +0100

    Optimize DB-Init.js

--- private/js/04-DB-Init.js
+++ private/js/04-DB-Init.js
@@ -1,9 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const dblocation = process.env.NODE_ENV ? `${__dirname}/../../db.sqlite3` : `${__dirname}/../../../db.sqlite3`
-    , db = new sqlite3.Database(dblocation, err => {
-        if (err) {
-            return console.error(err.message)
-        } else {
-            //console.info('Database Option connection alive')
-        }
-    })
+    // Define the Database location following if it is in the build or inside dev environment
+    , dblocation = process.env.NODE_ENV ? `${__dirname}/../../db.sqlite3` : `${__dirname}/../../../db.sqlite3`
+    // Run SQLIte database connection
+    , db = new sqlite3.Database(dblocation, err => err ? console.error(err.message) : true)
